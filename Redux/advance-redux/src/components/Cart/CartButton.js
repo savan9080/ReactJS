8f6a5e24bb2b4fa3692b23b4aaca03eefeeed294
savan patel <savan9080@gmail.com>
2021-06-26 23:18:01 +0530

    Advanced Redux Started 26/6/21

---
+++ Redux/advance-redux/src/components/Cart/CartButton.js
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleCardHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+  return (
+    <button className={classes.button} onClick={toggleCardHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
